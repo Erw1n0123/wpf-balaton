e779081a0c57a91a57ba92831f63ddbf46baf32e
Erw1n0123 <kisserikervin@gmail.com>
2023-05-13 15:41:23 +0200

    fel

---
+++ BalatonWPF/Epitmeny.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalatonWPF
+{
+    internal class Epitmeny
+    {
+        public int adoszam { get; set; }
+        public string utca { get; set; }
+        public string hazszam { get; set; }
+        public string adosav { get; set; }
+        public int alapterulet { get; set; }
+
+        public Epitmeny(string sor)
+        {
+            string[] splitted = sor.Split(' ');
+            this.adoszam = int.Parse(splitted[0]);
+            this.utca = splitted[1];
+            this.hazszam = splitted[2];
+            this.adosav = splitted[3];
+            this.alapterulet = int.Parse(splitted[4]);
+        }
+    }
+}
---
+++ BalatonWPF/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace BalatonWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Epitmeny> epitmenylist = new List<Epitmeny>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dictionary<string, int> Adosav = new Dictionary<string, int>();
+
+            StreamReader r = new StreamReader("utca.txt");
+            string line = r.ReadLine();
+            string[] splitted = line.Split(' ');
+            Adosav.Add("A", int.Parse(splitted[0]));
+            Adosav.Add("B", int.Parse(splitted[1]));
+            Adosav.Add("C", int.Parse(splitted[2]));
+            while (!r.EndOfStream)
+            {
+                epitmenylist.Add(new Epitmeny(r.ReadLine()));
+            }
+            r.Close();
+            Datagrid.ItemsSource = epitmenylist;
+            Kategoria.Items.Add("A");
+            Kategoria.Items.Add("B");
+            Kategoria.Items.Add("C");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            epitmenylist[Datagrid.SelectedIndex].adosav = Kategoria.Text;
+            Datagrid.Items.Refresh();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+StreamWriter w = new StreamWriter("modositottadok.txt");
+            for (int i = 0; i < epitmenylist.Count; i++)
+            {
+                w.WriteLine($"{epitmenylist[i].adoszam} {epitmenylist[i].utca} {epitmenylist[i].hazszam} {epitmenylist[i].adosav} {epitmenylist[i].alapterulet}");
+            }
+            w.Close();
+                MessageBox.Show("Sikeres mentés!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
